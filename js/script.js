999ecec13a637d81e4f7a778442cc047c31eeb78
pavlo20031 <lesivpavlo999@gmail.com>
2026-02-12 21:14:03 +0200

    commit67

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Numbers
+const num = [3, 6, 5];
+num[1] = 10;
+console.log(num);
+
+// Strings
+const str = ["forte", "piano", "mezzoforte"];
+str[3] = "mezzopiano";
+console.log(str);
+
+// Operations
+const sum = [6, 8, 3, 6];
+let total = 0;
+for (const elem of sum) {
+    total += elem;
+}
+console.log(total);
+
+// For
+const numbers = [1, 2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// Symbols
+const symbols = ["Igor", "Colt", "Shelly", "Bull", "Angelo"];
+for (const elem of symbols) {
+    if (elem.length >= 5) {
+        console.log(elem);
+    } else {
+        continue;
+    }
+}
+
+// Search
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const elem of array) {
+    if (elem % 2 === 0) {
+        console.log(elem);
+    } else {
+        continue;
+    }
+}
